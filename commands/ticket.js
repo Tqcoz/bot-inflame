e655bfc82b60b5429e85930b1706a5b1fae69fe5
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-27 13:15:50 -0400

    Create ticket.js

---
+++ commands/ticket.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");
+const prefix = ".";
+module.exports.run = async (bot, message, args) => {
+if(!message.content.startsWith(prefix)) return ;
+    let reason = args.join(" ").slice(22);
+
+    let reportEmbed = new discord.RichEmbed()
+    .setTitle("**New Ticket**")
+    .setColor("#bf42f4")
+    .addField("Reported By", `${message.author} with user ID ${message.author.id}.`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", reason);
+
+    let reportschannel = message.guild.channels.find(`name`, "tickets");
+    if(!reportschannel) return message.channel.send("Couldn't locate a tickets channel. Please contact a server manager. The channel must be named: `tickets`.")
+
+        message.delete().catch(O_o=>{});
+        reportschannel.send(reportEmbed)
+        if(reportschannel) return message.channel.send("Ticket sent, thanks!")
+
+}
+
+module.exports.help = {
+    name: "ticket"
+
+}
