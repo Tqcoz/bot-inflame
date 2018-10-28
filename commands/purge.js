487c2e1981ca2e9ac03f11964a51ea74092bb14d
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-28 16:14:24 -0400

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,16 +1,11 @@
 const Discord = require("discord.js");
-const moment = require("moment")
+
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You must have the `MANAGE_MESSAGES` Permission from either `Vixel` or `pvpar` to purge messages.");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
   if(!args[0]) return message.channel.send("no");
   message.channel.bulkDelete(args[0]).then(() => {
-  let purge = new Discord.RichEmbed()
-    .setDescription("Purging ${args[0]} Messages.")
-    .setColor("#a22c29")
-    .setThumbnail('https://ubisafe.org/images/flaming-clipart-drawn-4.jpg')
-    .addField("Purged By", message.member)
-    .addField("Purged on", message.createdAt);
+  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
 }
