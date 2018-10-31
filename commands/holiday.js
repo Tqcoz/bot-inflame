e07f4d89dd4925150976feaade6970c2da777871
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-31 19:00:44 -0400

    Update holiday.js

--- commands/holiday.js
+++ commands/holiday.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 const prefix = ".";
+const emoji = guild.emojis.first();
+
 module.exports.run = async (bot, message, args) => {
 
         if(message.content.startsWith(prefix)) { 
-          message.channel.send('Happy Halloween ' + (message.author.username) + (message.emoji('👻'));
+          message.channel.send('Happy Halloween ' + (message.author.username) + emoji.name('ghost'));
           message.react('🎃');
         }
     }  
