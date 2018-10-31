d7b391eae26914b97e46691cc5e85d0539ac36cb
pvpar <44278030+pvpar@users.noreply.github.com>
2018-10-31 16:16:09 -0700

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const prefix = ".";
 module.exports.run = async (bot, message, args) => {
 
-    if(message.author.hasPermission === ('ADMINISTRATOR')) return;
+    if(!message.author.hasPermission === ('ADMINISTRATOR')) return;
        message.channel.send('Restarting!'); 
        process.exit(0)
 
