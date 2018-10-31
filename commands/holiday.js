9311278c7816535d54c5551c2650ce95d29030db
pvpar <44278030+pvpar@users.noreply.github.com>
2018-10-31 16:14:05 -0700

    Update holiday.js

--- commands/holiday.js
+++ commands/holiday.js
@@ -4,7 +4,7 @@ const prefix = ".";
 module.exports.run = async (bot, message, args) => {
         
         if(message.content.startsWith(prefix)) { 
-          message.channel.send('Happy Halloween ' + (message.author.username) + ':ghost:' );
+          message.channel.send('Happy Halloween ' + message.author.username + ':ghost:' );
           message.react('🎃');
         }
     }  
