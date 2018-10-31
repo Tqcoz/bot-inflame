85bedc88a0b18bebb08e55d226e36c72d036d27a
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-30 21:42:28 -0400

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.content.startsWith(prefix)) return;
+         dmchannel.author.send(invite.url);
+
+}
+
+module.exports.help = {
+  name: "invite"
+}
