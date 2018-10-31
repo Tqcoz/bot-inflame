3d5a66328e7799d779028c9425a0db4a640352e7
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-31 18:47:27 -0400

    Update holiday.js

--- commands/holiday.js
+++ commands/holiday.js
@@ -2,12 +2,11 @@ const Discord = require("discord.js");
 const prefix = ".";
 module.exports.run = async (bot, message, args) => {
 
-        if(message.content.startsWith(prefix)) return; 
+        if(message.content.startsWith(prefix)) { 
           message.channel.send('Happy Halloween ' + (message.author.username) 👻');
           message.react('🎃');
-        
-      
-
+        }
+    }  
 module.exports.help = {
   name: "halloween"
 }
