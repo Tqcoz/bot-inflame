3d283b7f06a6b41817f848d5159add4bd0868461
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-30 23:28:09 -0400

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,8 +1,13 @@
-const prefix = " ";
+const Discord = require("discord.js");
+const prefix = ".";
 module.exports.run = async (bot, message, args) => {
 
+    if(message.author.hasPermission === ('ADMINISTRATOR')) return;
+       message.channel.send('Restarting!') 
     process.exit(1)
 
+}
+
 module.exports.help = {
-  name: "InflameMC is nice"
+  name: "restart"
 }
