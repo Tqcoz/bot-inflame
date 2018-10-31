2ecbdec60749e5cc256ed113dee768628c047750
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-30 22:33:44 -0400

    Create dm.js

---
+++ dm.js
@@ -0,0 +1,22 @@
+const Disc = require('discord.js');
+const readline = require('readline');
+
+
+if(message.channel.type === 'dm'){
+        console.log("[" + message.author.username + "]: " + message.content) //Message from : Message
+        
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+  
+        rl.question('REPLY TO ' + message.author.username + ': ', (answer) => {
+        message.author.send(`${answer}`);
+        rl.close();
+     });
+     }
+});
+
+module.exports.help = {
+  name: "dm"
+}
