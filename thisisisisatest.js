929810c0435019a8b85a3f5bdfcc46898ba3eeea
pvpar <44278030+pvpar@users.noreply.github.com>
2018-10-31 15:24:16 -0700

    Update thisisisisatest.js

--- thisisisisatest.js
+++ thisisisisatest.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       if(!message.content.startsWith(prefix)) return;
-        message.member.send('YOUR A NERD')
+        message.member.send("YOU'RE A NERD")
 }
 
 module.exports.help = {
