fb10a45603b4262ea9b31d7307947b0530b59d5b
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-10-30 22:57:24 -0400

    Create thisisatest.js

---
+++ commands/thisisatest.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+      if(!message.content.startsWith(prefix)) return;
+        message.member.send('YOUR A NERD')
+}
+
+module.exports.help = {
+  name: "nerd"
+}
