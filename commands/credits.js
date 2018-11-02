c324291afe24051b500a1b8cfe1f3a9b60ae2114
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-02 11:37:30 -0700

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,2 +1,24 @@
-{
+
+const Discord = require("discord.js");
+const fs = require("fs");
+const coins = require("../coins.json");
+const prefix = ".";
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.content.startsWith(prefix)) return ;
+
+    let creditsEmbed = new Discord.RichEmbed()
+    .setDescription(":credit_card: **InflameBot Creation Credits** :credit_card:")
+    .setTitle("Our GitHub page for reference")
+    .addField("Coding", "pvpar#0001 and Vixel#6105")
+    .addField("Ideas", "pvpar#0001 and Vixel#6105")
+    .addField("Art", "Vixel#6105 and non-copyrighted images from the internet")
+    .setURL("https://github.com/Tqcoz/inflame-bot")
+    .setColor("#ff0000");
+
+    message.channel.send(creditsEmbed);
+}
+
+module.exports.help = {
+  name: "credits"
 }
