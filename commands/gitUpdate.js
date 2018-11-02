c84f9c18801f03988c4cade7750e5009601e263f
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-02 16:37:24 -0700

    Update gitUpdate.js

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     if(!message.author.hasPermission === ('ADMINISTRATOR')) return ;
       message.channel.bulkDelete(1).then(() => {
           let updater = args[0];
-    let updateMsg = args.join(" ").slice(5);
+    let updateMsg = args.join(" ").slice(args[0]);
     let gitUpdateEmbed = new Discord.RichEmbed()
     .setDescription(":arrow_up: **New GitHub Update** :arrow_up:")
     .setTitle("Our GitHub Page")
