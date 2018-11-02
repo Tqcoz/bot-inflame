a77eb7c2ba49492a916233ad55a3347c595d8962
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-02 11:51:39 -0700

    Update holiday.js

--- commands/holiday.js
+++ commands/holiday.js
@@ -3,11 +3,11 @@ const prefix = ".";
 
 module.exports.run = async (bot, message, args) => {
         
-        if(message.content.startsWith(prefix)) return;
+        if(!message.content.startsWith(prefix)) return;
           message.channel.send('Happy Halloween ' + (message.author.username) + ' :ghost:' );
           message.react('🎃');
           message.edit('This Halloween is gonna haunt you for ever! I have a dig bick!');
     }  
 module.exports.help = {
-  name: "holiday"
+  name: "halloween"
 }
