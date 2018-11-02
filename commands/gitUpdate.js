2078434abb14c292c7c0362937b9d896aacb2080
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-02 16:46:35 -0700

    Update gitUpdate.js

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -7,11 +7,11 @@ module.exports.run = async (bot, message, args) => {
     if(!message.author.hasPermission === ('ADMINISTRATOR')) return ;
       message.channel.bulkDelete(1).then(() => {
           let updater = args[0];
-    let updateMsg = args.join(" ").slice(args[0]);
+    let updateMsg = args.join(" ").slice(5);
     let gitUpdateEmbed = new Discord.RichEmbed()
     .setDescription(":arrow_up: **New GitHub Update** :arrow_up:")
     .setTitle("Our GitHub Page")
-    .addField("Update:", updateMsg -- updater)
+    .addField("Update:", updateMsg)
     .setColor("#228B22")
     .setThumbnail("http://pluspng.com/img-png/github-octocat-logo-vector-png-png-ico-icns-svg-more-512.png")
     .setURL("https://github.com/Tqcoz/inflame-bot")
