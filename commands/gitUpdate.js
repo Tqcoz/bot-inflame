319a3eb0599f813428e08cef01c7ca488981f903
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-01 22:58:04 -0700

    Update gitUpdate.js

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     let updateMsg = args[0];
     let gitUpdateEmbed = new Discord.RichEmbed()
     .setDescription(":arrow_up: **New GitHub Update** :arrow_up:")
-    .setTitle("Our GitHub Paage
+    .setTitle("Our GitHub Page")
     .addField("Update:", updateMsg)
     .setColor("#228B22")
     .setThumbnail("http://pluspng.com/img-png/github-octocat-logo-vector-png-png-ico-icns-svg-more-512.png")
