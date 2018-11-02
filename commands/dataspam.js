fb6eb25a3510155f3709e3a1c1f64a0cb983a296
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-01 21:47:12 -0400

    Create dataspam.js

---
+++ commands/dataspam.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const api = "https://jsonplaceholder.typicode.com/posts"
+const sf = require("snekfetch")
+exports.run = (client, msg, args) => {
+let ch = message.guild.channels.find(`name`, "member-botspam");
+
+    sf.get(api).then(ch.send())
+  
+};
+
+module.exports.help = {
+  name: "botspam"
+}
