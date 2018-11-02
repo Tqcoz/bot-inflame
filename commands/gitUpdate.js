781662ed60aa4ce147d7bf2e72d211e97cdf7960
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-01 22:57:43 -0700

    Update gitUpdate.js

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -8,10 +8,13 @@ module.exports.run = async (bot, message, args) => {
     let updateMsg = args[0];
     let gitUpdateEmbed = new Discord.RichEmbed()
     .setDescription(":arrow_up: **New GitHub Update** :arrow_up:")
-    .addField("Update", updateMsg)
+    .setTitle("Our GitHub Paage
+    .addField("Update:", updateMsg)
     .setColor("#228B22")
     .setThumbnail("http://pluspng.com/img-png/github-octocat-logo-vector-png-png-ico-icns-svg-more-512.png")
+    .setURL("https://github.com/Tqcoz/inflame-bot")
     .setFooter("Visit our official GitHub page [here](https://github.com/Tqcoz/inflame-bot).");
+    
     
     message.channel.send(gitUpdateEmbed);
 
