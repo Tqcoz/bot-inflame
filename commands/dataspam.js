c1487d771bcf4d3d497f4beeea2283fd5e6bd404
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-01 22:26:07 -0700

    Update dataspam.js

--- commands/dataspam.js
+++ commands/dataspam.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const api = "https://jsonplaceholder.typicode.com/posts"
 const sf = require("snekfetch")
-exports.run = (client, msg, args) => {
+module.exports.run = (client, msg, args) => {
 const ch = message.guild.channels.find(`name`, "member-botspam");
 
     sf.get(api).then(ch.send())
