6756f204ad98a5bcee4542c9bf66870601d6c460
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-02 23:34:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("The InflameMC Discord", {type: "WATCHING"});
+  bot.user.setActivity("an Island far away!", {type: "WATCHING"});
 
 });
 
