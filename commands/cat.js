e23c53d6e9a84acdaf2eaeded2b15586d7871894
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-04 19:25:55 -0800

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,24 +1,24 @@
-
 const Discord = require("discord.js");
 const fs = require("fs");
 const coins = require("../coins.json");
-const prefix = ".";
+const prefix = ">";
 const superAgent = require("superagent");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.content.startsWith(prefix)) return ;
+       if(message.author.id === vix) return message.channel.send("You don't get cats.");
     let {body} = await superAgent
-    .get(`http://random.cat/meow`);
+    .get(`http://aws.random.cat//meow`);
     
-    let dogEmbed = new Discord.RichEmbed()
+    let catEmbed = new Discord.RichEmbed()
     .setTitle("Source")
-    .setDescription(":cat: **A random cat for you!** :cat:")
+    .setDescription(":cat: **A random cat!** :cat:")
     .setImage(body.file)
-    .setColor("#ff9900")
-    .setURL("https://random.cat")
+    .setColor("#68f468")
+    .setURL("http://random.cat")
     .setFooter(`For ${message.author.username}!`);
     
-    message.channel.send(dogEmbed);
+    message.channel.send(catEmbed);
 }
 
 module.exports.help = {
