6e854c20e9a1f453ef469084a86c223691a2ed1b
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-09 19:05:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,20 +24,6 @@ fs.readdir("./commands/", (err, files) => {
   });
 });
 
-
-  if(message.channel.type === "dm") {
-      let embed = new Discord.RichEmbed()
-      .setTimestamp()
-      .setTitle("DM of the Bot")
-      .addField(`Sent By:`,`<@${message.author.id}>`)
-      .setColor("RANDOM")
-      .setThumbnail(message.author.displayAvatarURL)
-      .addField(`Message: `,message.content)
-      .setFooter(`DM Bot Messages | DM Logs`)
-   
-    bot.users.get("402522093557645322", "284805484991610883").send(embed)
-  }
-
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
