b77f6c71df41cbbb027d88c5cffacf1111d76b6e
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-09 19:32:25 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,63 +1,24 @@
 const moment = require("moment");
 
-module.exports.run = async (client, message, args) => {
-
-    message.channel.send(`Here is the different information of **${message.guild.name}**`, {
-        embed: {
-            color: 0xDF9C9D,
-            author: {
-                name: message.author.username,
-                icon_url: message.author.displayAvatarURL()
-            },
-            thumbnail: {
-                url: message.guild.iconURL()
-            },
-            fields: [{
-                    name: "• name:",
-                    value: `${message.guild.name}`,
-                    inline: true
-                }, {
-                    name: "• ID:",
-                    value: `${message.guild.id}`,
-                    inline: true
-                }, {
-                    name: "• Crated at:",
-                    value: moment(message.guild.createdAt).format("LL"),
-                    inline: true
-                }, {
-                    name: "• Owner:",
-                    value: message.guild.owner.user.tag,
-                    inline: true
-                }, {
-                    name: "• Members:",
-                    value: `${message.guild.memberCount}`,
-                    inline: true
-                }, {
-                    name: "• Last members:",
-                    value: `${Array.from(message.channel.guild.members.values()).sort((a, b) => b.joinedAt - a.joinedAt).map(m => `<@!${m.id}>`).splice(0, 1)}`,
-                    inline: true
-                }, {
-                    name: "• Channel",
-                    value: `**${message.guild.channels.filter(channel => channel.type === 'text').size}** text - **${message.guild.channels.filter(channel => channel.type === 'voice').size}** audio`,
-                    inline: true
-                }, {
-                    name: "• AFK channel",
-                    value: `${message.guild.afkChannel}`,
-                    inline: true
-                }, {
-                    name: `• Roles - **${message.channel.guild.roles.size}**:`,
-                    value: message.guild.roles.array().map(g => g).join(', '),
-                    inline: true
-                }, {
-                    name: `• Emojies - **${message.channel.guild.emojis.size}**:`,
-                    value: `${message.guild.emojis.map(e => e).join(', ')}`,
-                    inline: true
-                }
-            ]
-        }
-    })
+module.exports.run = (client, message, args) => {
+    switch(args[0]){
+        case 'p': //setting activity to "playing"
+        if(message.author.id != '402522093557645322')return message.channel.send('You cant do that')
+        client.user.setActivity(args.splice(1).join(' '), {type: 'playing'});
+        message.channel.send('**Playing** status ready');
+        break;
+        case 'w': //setting activity to "watching"
+        if(message.author.id != '402522093557645322')return message.channel.send('You cant do that')
+        client.user.setActivity(args.splice(1).join(' '), {type: 'watching'});
+        message.channel.send('**Wathcing** status ready')
+        break;
+        case 'l': //setting activity to "listening"
+        if(message.author.id != '402522093557645322')return message.channel.send('You cant do that')
+        client.user.setActivity(args.splice(1).join(' '), {type: 'listening'});
+        message.channel.send('**Listening** status ready');
+        break;
+    }
 }
-
 module.exports.help = {
-    name: "sinfo",
+    name: "status",
 }
