e1a088b01653e4b548ac609b4cbcfc9098f2ff83
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-10 09:41:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("InflameMC Discord", {type: "WATCHING"});
+  bot.user.setActivity('Updates for MineTex', { url: 'https://minetex.xyz', type: 'STREAMING'})
 
 });
 
