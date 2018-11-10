82a120b1123a454659c0c997442d47444ecce9ff
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-09 17:40:50 -0800

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,31 @@
+
+const discord = require("discord.js");
+const prefix = ".";
+module.exports.run = async (bot, message, args) => {
+    if(!message.content.startsWith(prefix)) return ;
+    let reason = args.join(" ");
+message.delete();
+    let reportEmbed = new discord.RichEmbed()
+    .setTitle("**New Suggestion**")
+    .setColor("#009fff")
+    .addField("Suggested By", `${message.author} with user ID ${message.author.id}.`)
+    .addField("Suggestion", reason)
+    .setFooter("React to vote on this idea!");
+    let suggestchannel = message.guild.channels.find(`name`, "suggestions");
+    if(!suggestchannel) return message.channel.send("Couldn't locate a suggestions channel. Please contact a server manager. The channel must be named: `suggestions`.")
+    let msg = await reportschannel.send(reportEmbed)
+        .then(function (msg) {
+            msg.react("❎");
+            msg.react("✅");
+            message.delete({timeout: 1000});
+            }).catch(function(error) {
+            console.log(error);
+        });
+        if(suggestchannel) return message.channel.send(":ok_hand: Suggestion sent, thanks!");
+
+}
+
+module.exports.help = {
+    name: "suggest"
+
+}
