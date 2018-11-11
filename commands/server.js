0c0cab41c08656d4cd12fb0e393fa91b04388390
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-10 19:15:21 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,25 +1,26 @@
 const moment = require("moment");
 const discord = require("discord.js");
+const admins = ['402522093557645322','284805484991610883'];
 
 module.exports.run = (client, message, args) => {
     switch(args[0]){
         case 'p':
-        if(message.author.id != ['402522093557645322', '284805484991610883'])return message.channel.send('You cant do that')
+        if(message.author.id != admins)return message.channel.send('You cant do that')
         client.user.setActivity(args.splice(1).join(' '), {type: 'playing'});
         message.delete();
         break;
         case 'w':
-        if(message.author.id != ['402522093557645322', '284805484991610883'])return message.channel.send('You cant do that')
+        if(message.author.id != admins)return message.channel.send('You cant do that')
         client.user.setActivity(args.splice(1).join(' '), {type: 'watching'});
         message.delete();
         break;
         case 'l':
-        if(message.author.id != ['402522093557645322', '284805484991610883'])return message.channel.send('You cant do that')
+        if(message.author.id != admins)return message.channel.send('You cant do that')
         client.user.setActivity(args.splice(1).join(' '), {type: 'listening'});
         message.delete();
         break;
         case 'r':
-        if(message.author.id != ['402522093557645322', '284805484991610883'])return message.channel.send('You cant do that')
+        if(message.author.id != admins)return message.channel.send('You cant do that')
         client.user.setActivity(" the InflameMC Discord", {type: "WATCHING"});
         message.delete();
         break;
