3b4c2166c7ca81d0d3e60f6c0f615f59c3163a3e
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-11-12 20:01:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("GuildMemberAdd", member => {
 let woolcome = member.guild.channels.find("name", "welcome-spam");
   woolcome.send('OMG LOOK WHO JOINED, IT IS ${member}!')
-}
+});
 
 bot.on("ready", async () => {
 
