8a1dbb3c1673aa8e5c537480ac3c2335bae027bf
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 11:33:47 -0800

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
-module.exports.run = (client, bot, msg, args, message) => {
-  msg.channel.bulkDelete(1).then(() => {
+module.exports.run = (bot, args, message) => {
+  message.channel.bulkDelete(1).then(() => {
     
   let embedContent = args.join(" ");
   const embed = new Discord.RichEmbed()
     .setDescription(embedContent)
     .setColor("#f575a0");
-  msg.channel.send(embed);
+  message.channel.send(embed);
   });
 }
 
