56ab7436c6422b0794f2eb6edc80a4895ae6a7a7
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 11:26:32 -0800

    00f

--- commands/clout.js
+++ commands/clout.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const prefix = "!";
+const prefix = ".";
 
 module.exports.run = async (bot, message, client, args) => {
  if(!message.content.startsWith(prefix)) return ;   
