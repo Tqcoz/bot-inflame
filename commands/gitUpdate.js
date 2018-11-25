649744da3940c101f3453604d6ae845d702e6a54
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 11:35:06 -0800

    fixed bc i was bad

--- commands/gitUpdate.js
+++ commands/gitUpdate.js
@@ -6,8 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(!message.content.startsWith(prefix)) return ;
     if(!message.author.hasPermission === ('ADMINISTRATOR')) return ;
       message.channel.bulkDelete(1).then(() => {
-          let updater = args[0];
-    let updateMsg = args.join(" ").slice(5);
+    let updateMsg = args.join(" ");
     let gitUpdateEmbed = new Discord.RichEmbed()
     .setDescription(":arrow_up: **New GitHub Update** :arrow_up:")
     .setTitle("Our GitHub Page")
@@ -15,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#228B22")
     .setThumbnail("http://pluspng.com/img-png/github-octocat-logo-vector-png-png-ico-icns-svg-more-512.png")
     .setURL("https://github.com/Tqcoz/inflame-bot")
-    .setFooter(`Update logged and committed by ${updater}.`);
+    .setFooter(`Update logged and committed by ${message.author.username}.`);
     
     
     message.channel.send(gitUpdateEmbed);
