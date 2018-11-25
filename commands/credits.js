9cb1730698f22488b0aa9360ddc82cb5d3b2d9b5
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 11:07:43 -0800

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -1,7 +1,6 @@
 
 const Discord = require("discord.js");
 const fs = require("fs");
-const coins = require("../coins.json");
 const prefix = ".";
 
 module.exports.run = async (bot, message, args) => {
