b4ae148de784abe4f313bc4990c8322079db3322
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 16:31:33 -0800

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -1,11 +1,17 @@
-const Discord = require("discord.js");
+ const Discord = require("discord.js");
 const prefix = ".";
-module.exports.run = async (bot, message, args) => {
-
-    if(message.content.startsWith(prefix)) return ;
-    if(!message.author.hasPermission === ('ADMINISTRATOR')) return ;
-     await  message.channel.send('Restarting!'); 
-       process.exit(1)
+module.exports.run = async (client, message, args) => {
+  if(!message.content.startsWith(prefix)) return ;
+  if (message.author.id === "284805484991610883") {
+    await message.reply("CloutBot is restarting.");
+    client.commands.forEach(async cmd => {
+      await client.unloadCommand(cmd);
+    });
+    process.exit(1);
+  } else {
+    message.reply("only the bot owner can restart CloutBot!");
+  }
+  
 
 }
 
