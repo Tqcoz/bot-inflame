b56c94e273cdd26fa533eb6c8b45e18b492128f8
pvpar <44278030+pvpar@users.noreply.github.com>
2018-11-25 16:30:41 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Insufficient permissions.");
+  if(!args[0]) return message.channel.send("Missings arguments.");
   message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
+  message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(2000));
 });
 
 }
