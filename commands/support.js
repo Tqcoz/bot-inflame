6bcda150eb732adb07b37fb5461b7c9430b5c98e
pvpar <44278030+pvpar@users.noreply.github.com>
2018-12-08 18:52:21 -0800

    added .support (beta)

---
+++ commands/support.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const prefix = ".";
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.content.startsWith(prefix)) return ;
+
+    await message.delete();
+    const filter = m => m.author.id === message.author.id;
+    message.channel.send("What do you need help with? You can send `cancel` at any time to cancel this appeal. __Hint:__ Use proper grammar. If your question is in a language other than English, it won't be reviewed.").then(q => q.delete(15000))
+    message.channel.awaitMessages(filter, {
+      max: 5,
+      time: 300000
+    }).then(collected => {
+      collected.delete(15000);
+      if (collected.first().content === 'cancel') {
+        return message.reply("Canceled.");
+      }
+  let appealContent = collected.first().content;
+  let supportEmbed = new Discord.RichEmbed()
+  .setDescription(`**SUPPORT QUESTION:**
+  
+  ${appealContent}
+  
+  Please reply to this person as soon as possible via ".message".`)
+
+      }).catch(err => {
+      message.reply("Cancelled...").then(r => r.delete(5000));
+      console.log("Time exceeded. Message await cancelled.");
+    });
+
+}
+
+module.exports.help = {
+  name: "support"
+}
