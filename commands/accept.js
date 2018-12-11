3e560182572a0710e798f3c3412776694321e8eb
pvpar <44278030+pvpar@users.noreply.github.com>
2018-12-10 17:39:25 -0800

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -9,10 +9,10 @@ let aUser = message.guild.member(message.mentions.users.first() || message.guild
 
 let embed = new Discord.RichEmbed()
 .setTitle("InflameMC")
-.addField(":x: | InflameMC Staff Application **denied**. Thanks for applying!")
+.addField(":x: | InflameMC Staff Application **denied**. Thanks for applying!", "You may apply again in 30 days.")
 .setFooter("join.inflamemc.com | Time: ${message.createdAt}")
 .setURL("https://minetex.xyz/flame")
-.setColor("#123cba");
+.setColor("#ffcccc");
 
 aUser.send(embed);
 
