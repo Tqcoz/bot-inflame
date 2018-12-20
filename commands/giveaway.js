f14a272774de53029c99bf009afaa43c1c201a16
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-12-19 20:17:46 -0500

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -2,28 +2,18 @@ const Discord = require("discord.js");
 const prefix = ".";
 
 module.exports.run = async (bot, message, client, args) => {
-   if(!message.content.startsWith(prefix)) return ;
-   let mAuth = message.author.username;
-   let mIcon = message.author.avatarURL;
-   let mDiscrim = message.author.discriminator;
-  let giveaway = message.guild.channels.find(`name`, "giveaway");
-   if(!message.channel === giveaway) return message.channel.send(":x: Please enter for the giveaway at #giveaway!");
-await message.delete();
+    let giveaway = message.guild.channels.find(`name`, "temp2");
+   let enter = new discord.RichEmbed()
+.setTitle("Giveaway")
+.setDescription("${message.author.username}${message.author.discriminator}")
+.setFooter("Time: ${message.createdAt}")
+.setColor("#006400");
 
- let giveawayChannel = message.guild.channels.find(`name`, "temp2");
- 
- let giveawayEmbed = new Discord.RichEmbed()
- .setTitle(`Giveaway ${mAuth}${mDiscrim}`)
- .setDescription(`${mAuth} just entered the Nitro Giveaway!`)
- .addField('InflameMC', 'join.inflamemc.com')
- .setFooter(` Discord: discord.io/inflamemc | Time: ${message.createdAt}`)
- .setColor("#d2b48c")    
- .setThumbnail(mIcon)
-     
- appealsChannel.send(giveawayEmbed);
-     message.channel.send(":ok_hand: Entered! Be sure to be active or you will not be spun!");
-});
- }
+giveaway.channel.send(enter)
+   message.channel.send(':white_check_mark: Entered');
+
+}
+   
 module.exports.help = {
   name:"enter"
 }
