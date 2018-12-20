727a9323ee65e19f128df005310cc5685b169d2b
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-12-19 20:02:35 -0500

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const prefix = ".";
 
-module.exports.run = async (bot, message, client, args) => {(
+module.exports.run = async (bot, message, client, args) => {
    if(!message.content.startsWith(prefix)) return ;
    let mAuth = message.author.username;
    let mIcon = message.author.avatarURL;
@@ -22,7 +22,7 @@ await message.delete();
      
  appealsChannel.send(giveawayEmbed);
      message.channel.send(":ok_hand: Entered! Be sure to be active or you will not be spun!");
-  });
+  };
  }
 module.exports.help = {
   name:"enter"
