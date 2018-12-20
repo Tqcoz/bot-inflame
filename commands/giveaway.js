12c6e2d9c1bb1eb24eaf8058b06a17feee19df79
Vixel V Vixel <33169104+Tqcoz@users.noreply.github.com>
2018-12-19 20:26:54 -0500

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 const prefix = ".";
 
 module.exports.run = async (bot, message, client, args) => {
+    
     let giveaway = message.guild.channels.find(`name`, "temp2");
-    let enter = new discord.RichEmbed()
+    const enter = new discord.RichEmbed()
     .setTitle("Giveaway")
     .setDescription("${message.author.username}${message.author.discriminator}")
     .setFooter("Time: ${message.createdAt}")
-    .setColor("#006400");
+    .setColor("#006400")
 
-    giveaway.channel.send(enter);
+    giveaway.channel.send(enter)
    message.channel.send(':white_check_mark: Entered');
 
 }
