97c57a04dce0afe70033840c738e7ffca22dced7
pvpar <44278030+pvpar@users.noreply.github.com>
2018-12-22 18:20:59 -0800

    Update giveaway.js

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 const prefix = ".";
 
 module.exports.run = async (bot, message, client, args) => {
-  
+  if(!message.content.startsWith(prefix)) return ;
     let giveaway = message.guild.channels.find(`name`, "temp2");
     const enter = new discord.RichEmbed()
     .setTitle("Giveaway")
     .setDescription("${message.author.username}${message.author.discriminator}")
     .setFooter("Time: ${message.createdAt}")
-    .setColor("#006400")
+    .setColor("#006400");
 
-    giveaway.send(enter)
+    giveaway.send(enter);
    message.channel.send(':white_check_mark: Entered');
 
 }
