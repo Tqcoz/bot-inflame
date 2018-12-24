356d642b3fb7c7018a37e424ab2f608be2db6d6f
pvpar <44278030+pvpar@users.noreply.github.com>
2018-12-23 18:29:45 -0800

    Update retell.js

--- commands/retell.js
+++ commands/retell.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
       if(!message.member.hasPermission("ADMINISTRATOR")) return;
-      const sayMessage = args.join(" ");
+      const sayMessage = args.join(" ").slice(6);
       message.delete().catch();
       message.channel.send(sayMessage);
 
