173af6bd7c8e84bb835203bf5c2e25a6a53e4a89
pvpar <44278030+pvpar@users.noreply.github.com>
2018-12-25 21:51:37 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Insufficient permissions.");
   if(!args[0]) return message.channel.send("Missings arguments.");
-  message.channel.bulkDelete(args.join(" ").slice(5)).then(() => {
+  let purgeAm = args.join(" ").slice(5) + 1;
+  message.channel.bulkDelete(purgeAm).then(() => {
   message.channel.send(`Cleared ${args.join(" ").slice(5)} messages.`).then(msg => msg.delete(2000));
 });
 
